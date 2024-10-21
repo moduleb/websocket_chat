b1938f3f546bb8c11b9c7e779f7666351d1215b3
a <a@as-MacBook-Pro.local>
2024-10-22 00:57:18 +0400

    начал делать отдельный эндпойнт для истории сообщений

---
+++ fastapi/app/api/messages.py
@@ -0,0 +1,24 @@
+import logging
+from typing import TYPE_CHECKING, Annotated
+
+from app.db.schemas.session import SessionData
+from app.services.servise_factory import ServiceFactory, get_service_factory
+from app.services.sessions.init import cookie
+from app.services.sessions.verifier import verifier
+from fastapi import APIRouter, Depends, Response, status
+
+if TYPE_CHECKING:
+    from app.db.models.user import User
+
+router = APIRouter()
+
+logger = logging.getLogger(__name__)
+
+
+@router.get("/", dependencies=[Depends(cookie)], status_code=status.HTTP_200_OK)
+async def messages(
+    service_factory: Annotated[ServiceFactory, Depends(get_service_factory)],
+    session_data: Annotated[SessionData, Depends(verifier)],
+):
+    """Возвращает историю сообщения двух пользователей."""
+
